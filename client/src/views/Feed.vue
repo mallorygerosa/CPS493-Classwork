67d18b946ba21043a877748f5d872fd87bfc7ed4
MalloryGerosa <mallorygerosa@gmail.com>
2021-10-14 15:25:14 -0400

    Added posts and useres

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -3,11 +3,25 @@
       <h1 class="title">
           Feed Page
       </h1>
+          <div class="columns">
+            <div class="colmn is-one-third is-offset-one-third">
+              <div class="post" v-for="p in posts" :key="p.src">
+<post :post="p" />
+              </div>
+            </div>
+          </div>
   </div>
 </template>
 
 <script>
+import Post from '../components/Post';
+import session from "../services/session";
+import {GetWall } from "../services/posts";
+
 export default {
+    data: ()=>({
+      posts:GetWall(session.user.handle)
+    })
 }
 </script>
 
